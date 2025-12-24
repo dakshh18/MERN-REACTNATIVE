41650bb86639f898aa22fa6c225c6d98ac730685
dakshh18 <pdaksh163@gmail.com>
2025-12-24 08:06:22 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
+
+const app = express();
+
+const __dirname = path.resolve();
+
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ message: "Server is running" });
+})
+// make our app ready fior deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    })
+}
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+});
