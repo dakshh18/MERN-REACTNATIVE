ad6402cc7a55e66eff67e6ea827f3d34801abd68
dakshh18ES <daksh.patel@elitewaresolutions.in>
2025-12-25 09:05:52 +0530

    changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <div>
       <h1>Home page</h1>
-      <SignedOut>
-        <SignInButton />
+      <SignedOut >
+        <SignInButton  mode='modal'/>
       </SignedOut>
-      <SignedIn>
-        <UserButton />
+      <SignedIn >
+        <UserButton mode='modal' />
       </SignedIn>
     </div>
   )
