184e983a1d211109e89d602405a665a0ff2e8dd9
dakshh18ES <daksh.patel@elitewaresolutions.in>
2025-12-30 08:29:37 +0530

    admin routes added

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import path from "path";
+
+
+const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        cb(null , `${Date.now()}-${file.originalname}`);
+    }
+})
+
+// filter the file types : jpeg , jpg , png , webp
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png|webp/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+    if(extname && mimetype){
+        cb(null , true);
+    }else{
+        cb(new Error("Unsupported file type"), false);
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 5, // 5MB
+    }
+})
+
+export default upload;
