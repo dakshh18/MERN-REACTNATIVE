dc4f42086aac55d65accc813c09434c1be60ea71
dakshh18ES <daksh.patel@elitewaresolutions.in>
2026-01-01 08:04:53 +0530

    chanegs and whole frontend

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,20 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+import * as Sentry from "@sentry/react";
+
+// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if(!PUBLISHABLE_KEY){
-  throw new Error('VITE_CLERK_PUBLISHABLE_KEY is not set');
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
 }
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  enableLogs: true,
+
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ClerkProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
